aaac1aca5d67eac2da5c0308e74ac86abf8d7990
Ariane <xu.ariane@hotmail.fr>
2020-04-06 15:46:18 +0100

    create db

--- src/main/resources/db/migration/V1__create_table.sql
+++ src/main/resources/db/migration/V1__create_table.sql
@@ -3,3 +3,19 @@ create table posts(
  title VARCHAR,
  content VARCHAR
 )
+
+--In db acebook,table Posts:
+ALTER TABLE posts ADD COLUMN date VARCHAR;
+ALTER TABLE posts ADD COLUMN time timestamp;
+
+--In db acebook-test,table posts:
+ALTER TABLE posts ADD COLUMN date VARCHAR;
+ALTER TABLE posts ADD COLUMN time timestamp;
+
+
+
+--Create new user table in acebook db:
+CREATE TABLE person(user_id SERIAL PRIMARY KEY, user_name VARCHAR(60), password VARCHAR);
+
+--Create new user table in acebook-test db:
+CREATE TABLE person(user_id SERIAL PRIMARY KEY, user_name VARCHAR(60), password VARCHAR);
